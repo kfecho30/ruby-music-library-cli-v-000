f71ec215518153df98bfd02aa4998660cd336202
Kelsey Fecho <kfecho30@gmail.com>
2018-03-16 18:11:49 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -71,7 +71,7 @@ class MusicLibraryController
     art = gets.strip
     artist = Artist.find_or_create_by_name(art)
     sorted = artist.sort_by {|song| song.name}
-    artist.songs.each do |song|
+    sorted.songs.each do |song|
       puts "#{num}. #{song.name} - #{song.genre}"
       num +=1
     end
