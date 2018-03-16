8e7694b543483c47e72e0e0fd3435266d27dd17e
Kelsey Fecho <kfecho30@gmail.com>
2018-03-16 18:24:16 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -90,7 +90,6 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     list_songs
     input = gets.strip
-    binding.pry
     list_songs.detect(input)
   end
 end
